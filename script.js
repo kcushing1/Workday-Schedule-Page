08635c5bee9da6fed00ec41b16384fdea2c9869c
Kasey Cushing <kasey.cushing@gmail.com>
2020-10-19 21:10:48 -0400

    time-color-class (past, present, future) displays in correct textarea for real time. localStorage displaying 'undefined' in blank textarea

--- script.js
+++ script.js
@@ -2,38 +2,82 @@
 const date = moment().format("Do MMMM YYYY")
 const dateDisplayed = $("#currentDay")
 dateDisplayed.text(date)
+let currentHour = moment().format("hA")
+console.log(currentHour)
 
 //set page to default Eastern Time
 //moment.tz.setDefault("America/New_York")
 
+//displayed hours in an array - military time
+const hoursArr = ["09","10","11","12","13","14","15","16","20","21","22"]
 
-//display hours
-const hoursArr = ["09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"]
+let testhrarr = moment(hoursArr[0], "HH:mm").format("hA")
+console.log(testhrarr)//9am
 
 function setSchedule(){
   
+  const coloredTextArea = $("textarea")
+
  //the time changes the class
+ //clear the time related classes
+
+ //moment("time", "format to read").format("desired format")
+ 
+addRealTime()
+ function addRealTime (){
+  clearTimeClass()
+
+   for (let j=0; j< hoursArr.length; j++){
+     let grabHour = moment(hoursArr[j]+":00","HH:mm")
+     let realTime = moment()
+     let presentHour = realTime.startOf("hour")
+
+     console.log(hoursArr[j], grabHour)
+
+     //if it has the correct class and the grabHour is in the present
+     if ($("textarea").hasClass("time" + hoursArr[j]) && grabHour.isSame(presentHour)){
+       $(".time"+hoursArr[j]+"").addClass("present")
+       console.log("the time is now")
+
+       //if it has the correct class and the grabHour is in the past
+     } else if ($("textarea").hasClass("time" + hoursArr[j]) && grabHour.isBefore(presentHour)){
+       $(".time"+hoursArr[j]+"").addClass("past")
+       console.log("this is the past")
+
+       //if it has the correct class and the grabHour is in the future
+     } else if ($("textarea").hasClass("time" + hoursArr[j]) && grabHour.isAfter(presentHour)){
+       $(".time"+hoursArr[j]+"").addClass("future")
+      console.log("this is the future")
+     }
+  }
+  
+ }
+ 
+ function clearTimeClass(){
+  coloredTextArea.removeClass("past")
+  coloredTextArea.removeClass("present")
+  coloredTextArea.removeClass("future")
+}
+
+ //displays scheduled events previously save
+ //event+i+00 names in military time each section
  function displayEvents (){
   for (let i=9; i< 18; i++){
     $(".event"+ i +"00").val(localStorage.getItem("event"+i))
-    console.log("display events for loop event"+i+"00")
+
+  //make undefined not display
+
+    console.log("you still need to make undefined not show")
   }
  }
  displayEvents()
-
- //add new schedule event in text area
-
- //show the text that has previously been stored to localStorage
- //.getItem
  
- //text saved to localStorage so it remains when refreshed
+ //text saved to localStorage
   function saveCalendarEvent(){
     for (let i=9; i< 18; i++){
       localStorage.setItem("event"+i, $(".event"+ i +"00").val())
-      console.log("save calendar events for loop event"+i+"00")
     }
-    
-  }
+      }
 
  //click button to save text
   $(".saveBtn").click(function(){
@@ -41,10 +85,7 @@ function setSchedule(){
     console.log("saveBtn connected")
   })
 
-
 }
 
 setSchedule()
-//let currentHour = moment().format("hA")
-
-console.log(date)
+console.log("don't forget to remove the extra hours in html!")
